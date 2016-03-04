6468626bdb4bfd233231e0809b309ccc00678abb
jungmtg <cardofang@gmail.com>
2016-03-04 13:38:51 +0800

    Test

---
+++ WorkShop1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WorkShop1.MvcApplication" Language="C#" %>
---
+++ WorkShop1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WorkShop1.Startup))]
+namespace WorkShop1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
